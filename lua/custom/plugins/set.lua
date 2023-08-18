6083f9a32765989a2880da55aee88d57905f3cb9
Emmanuel Chucks <echucks19@gmail.com>
2023-08-18 23:43:15 +0000

    fix: go to prev buffer not working

--- lua/custom/plugins/set.lua
+++ lua/custom/plugins/set.lua
@@ -7,7 +7,7 @@ vim.opt.autowriteall = true
 
 -- Switch buffers
 vim.keymap.set('n', '[b', '<cmd>bprevious<cr>', { desc = 'Go to previous buffer' })
-vim.keymap.set('n', ']b', 'cmd>bnext<cr>', { desc = 'Go to next buffer' })
+vim.keymap.set('n', ']b', '<cmd>bnext<cr>', { desc = 'Go to next buffer' })
 
 -- Auto	format on save
 vim.api.nvim_create_autocmd({ 'FocusLost', 'BufWritePost' }, {
