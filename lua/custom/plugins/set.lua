4f93eb8dc4fd12f37e5042bba895a9102b6ddfec
Emmanuel Chucks <echucks19@gmail.com>
2023-08-18 15:22:31 +0000

    format on focus lost and save

--- lua/custom/plugins/set.lua
+++ lua/custom/plugins/set.lua
@@ -5,9 +5,9 @@ vim.opt.relativenumber = true
 vim.opt.scrolloff = 8
 vim.opt.autowriteall = true
 
--- Auto save on focus lost
-vim.api.nvim_create_autocmd('FocusLost', {
-	group = vim.api.nvim_create_augroup 'AutoSaveAutogroup',
+-- Auto	format on save
+vim.api.nvim_create_autocmd({ 'FocusLost', 'BufWritePost' }, {
+	group = vim.api.nvim_create_augroup('FormatOnSaveAutoGroup', {}),
 	callback = function()
 		vim.api.nvim_cmd({ cmd = 'FormatWrite' }, {})
 	end,
