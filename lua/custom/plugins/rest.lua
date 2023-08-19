c1752eb51e13ff683c0af6997bc40ba60858a58f
Emmanuel Chucks <echucks19@gmail.com>
2023-08-19 00:54:10 +0000

    set up rest client

--- lua/custom/plugins/rest.lua
+++ lua/custom/plugins/rest.lua
@@ -4,8 +4,14 @@
 return {
 	'rest-nvim/rest.nvim',
 	config = function()
-		require('nvim-treesitter.configs').setup {
-			ensure_installed = { 'http', 'json' },
+		require('rest-nvim').setup {
+			result_split_horizontal = true,
+			result_split_in_place = true,
 		}
+
+		vim.keymap.set('n', '<leader>rr', require('rest-nvim').run, { desc = '[R]un [R]request' })
+		vim.keymap.set('n', '<leader>rp', function()
+			require('rest-nvim').run(true)
+		end, { desc = '[R]un [P]review' })
 	end,
 }
