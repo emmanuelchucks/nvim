fa517eb88325bbec4c2cd62ac0aa9cd3ebb3b196
Emmanuel Chucks <echucks19@gmail.com>
2023-09-27 23:57:39 +0000

    add more keymaps for gitsigns

--- init.lua
+++ init.lua
@@ -125,6 +125,13 @@ require('lazy').setup({
       },
       on_attach = function(bufnr)
         vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'Preview git hunk' })
+        vim.keymap.set('n', '<leader>hr', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'Reset git hunk' })
+        vim.keymap.set('v', '<leader>hr', function()
+          require('gitsigns').reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
+        end, { buffer = bufnr, desc = 'Reset git hunk' })
+        vim.keymap.set('n', '<leader>hb', function()
+          require('gitsigns').blame_line { full = true }
+        end, { buffer = bufnr, desc = 'Git blame current line' })
 
         -- don't override the built-in and fugitive keymaps
         local gs = package.loaded.gitsigns
