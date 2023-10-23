bbe48efe1d4d42dbe269794505b35e8966121ce2
Emmanuel Chucks <echucks19@gmail.com>
2023-10-23 06:23:56 +0000

    feat(formatting): replace formatter with conform

---
+++ lua/custom/plugins/conform.lua
@@ -0,0 +1,26 @@
+-- conform.lua
+--
+
+return {
+	'stevearc/conform.nvim',
+	config = function()
+		require('conform').setup {
+			formatters_by_ft = {
+				javascript = { 'eslint_d', 'prettierd' },
+				javascriptreact = { 'eslint_d', 'prettierd' },
+				typescript = { 'eslint_d', 'prettierd' },
+				typescriptreact = { 'eslint_d', 'prettierd' },
+				astro = { 'eslint_d', 'prettierd' },
+				lua = { 'stylua' },
+			},
+		}
+
+		-- Auto	format on save
+		vim.api.nvim_create_autocmd('BufWritePre', {
+			group = vim.api.nvim_create_augroup('FormatOnSaveAutoGroup', { clear = true }),
+			callback = function()
+				require('conform').format()
+			end,
+		})
+	end,
+}
