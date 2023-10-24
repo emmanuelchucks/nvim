809f9af5083495c2babb50b0d3cc10e5bd44a290
Emmanuel Chucks <echucks19@gmail.com>
2023-10-24 21:07:07 +0000

    feat(conform): auto run slow formatters async

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -29,15 +29,28 @@ return {
 			},
 		}
 
-		-- Auto	format on save
-		vim.api.nvim_create_autocmd('BufWritePre', {
-			group = vim.api.nvim_create_augroup('FormatOnSaveAutoGroup', { clear = true }),
-			callback = function()
-				--   if buffer is not empty
-				if vim.fn.getline(1) ~= '' then
-					require('conform').format()
+		-- Automatically run slow formatters async
+		local slow_format_filetypes = {}
+		require('conform').setup {
+			format_on_save = function(bufnr)
+				if slow_format_filetypes[vim.bo[bufnr].filetype] then
+					return
 				end
+				local function on_format(err)
+					if err and err:match 'timeout$' then
+						slow_format_filetypes[vim.bo[bufnr].filetype] = true
+					end
+				end
+
+				return { timeout_ms = 200, lsp_fallback = true }, on_format
 			end,
-		})
+
+			format_after_save = function(bufnr)
+				if not slow_format_filetypes[vim.bo[bufnr].filetype] then
+					return
+				end
+				return { lsp_fallback = true }
+			end,
+		}
 	end,
 }
