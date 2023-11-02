ede93765f4f4117634a715b976b750dfc1f93ba2
Emmanuel Chucks <echucks19@gmail.com>
2023-11-02 22:52:02 +0000

    chore(format): rearrange code block

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -15,6 +15,16 @@ vim.api.nvim_create_autocmd("FileType", {
 	end,
 })
 
+-- Close some filetypes with <q>
+vim.api.nvim_create_autocmd("FileType", {
+	group = augroup("close_with_q"),
+	pattern = { "help", "fugitive" },
+	callback = function(event)
+		vim.bo[event.buf].buflisted = false
+		vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+	end,
+})
+
 -- Auto create dir when saving a file, in case some intermediate directory does not exist
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 	group = augroup("auto_create_dir"),
@@ -47,16 +57,6 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 		if mark[1] > 0 and mark[1] <= lcount then
 			pcall(vim.api.nvim_win_set_cursor, 0, mark)
 		end
-	end,
-})
-
--- Close some filetypes with <q>
-vim.api.nvim_create_autocmd("FileType", {
-	group = augroup("close_with_q"),
-	pattern = { "help", "fugitive" },
-	callback = function(event)
-		vim.bo[event.buf].buflisted = false
-		vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
 	end,
 })
 
