9d90e9b2ec6150306d5261bb04b56c6df1e09e53
Emmanuel Chucks <echucks19@gmail.com>
2023-11-03 02:19:31 +0000

    refactor(telescope): move all code under one file
    - using command kinda launches faster

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -113,30 +113,6 @@ return {
 	},
 
 	{
-		-- Fuzzy Finder (files, lsp, etc)
-		"nvim-telescope/telescope.nvim",
-		lazy = true,
-		branch = "0.1.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			-- Fuzzy Finder Algorithm which requires local dependencies to be built.
-			-- Only load if `make` is available. Make sure you have the system
-			-- requirements installed.
-			{
-				"nvim-telescope/telescope-fzf-native.nvim",
-				build = "make",
-				cond = function()
-					return vim.fn.executable("make") == 1
-				end,
-				config = function()
-					-- Enable telescope fzf native, if installed
-					pcall(require("telescope").load_extension, "fzf")
-				end,
-			},
-		},
-	},
-
-	{
 		"rest-nvim/rest.nvim",
 		ft = "http",
 		keys = {
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,8 +1,6 @@
 -- telescope.lua
 --
 
-local builtin = require("telescope.builtin")
-
 local prefix = "<leader>f"
 require("which-key").register({
 	[prefix] = { name = "Find", _ = "which_key_ignore" },
@@ -10,18 +8,36 @@ require("which-key").register({
 
 return {
 	"nvim-telescope/telescope.nvim",
+	branch = "0.1.x",
+	cmd = { "Telescope" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		-- Fuzzy Finder Algorithm which requires local dependencies to be built.
+		-- Only load if `make` is available.
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+			cond = function()
+				return vim.fn.executable("make") == 1
+			end,
+			config = function()
+				-- Enable telescope fzf native, if installed
+				pcall(require("telescope").load_extension, "fzf")
+			end,
+		},
+	},
 	keys = {
-		{ "<leader><space>", builtin.git_files, desc = "Find git files" },
-		{ prefix .. "/", builtin.current_buffer_fuzzy_find, desc = "Find in current buffer" },
-		{ prefix .. "o", builtin.oldfiles, desc = "Find recent files" },
-		{ prefix .. "f", builtin.find_files, desc = "Find files" },
-		{ prefix .. "g", builtin.live_grep, desc = "Find by grep" },
-		{ prefix .. "b", builtin.buffers, desc = "Find buffers" },
-		{ prefix .. "d", builtin.diagnostics, desc = "Find diagnostics" },
-		{ prefix .. "h", builtin.help_tags, desc = "Find help" },
-		{ prefix .. "r", builtin.resume, desc = "Find resume" },
-		{ prefix .. "w", builtin.grep_string, desc = "Find word by grep" },
-		{ prefix .. "s", builtin.spell_suggest, desc = "Find spelling" },
-		{ prefix .. "k", builtin.keymaps, desc = "Find keymaps" },
+		{ "<leader><space>", "<cmd>Telescope git_files<cr>", desc = "Find git files" },
+		{ prefix .. "/", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Find in current buffer" },
+		{ prefix .. "o", "<cmd>Telescope oldfiles<cr>", desc = "Find recent files" },
+		{ prefix .. "f", "<cmd>Telescope find_files<cr>", desc = "Find files" },
+		{ prefix .. "g", "<cmd>Telescope live_grep<cr>", desc = "Find by grep" },
+		{ prefix .. "b", "<cmd>Telescope buffers<cr>", desc = "Find buffers" },
+		{ prefix .. "d", "<cmd>Telescope diagnostics<cr>", desc = "Find diagnostics" },
+		{ prefix .. "h", "<cmd>Telescope help_tags<cr>", desc = "Find help" },
+		{ prefix .. "r", "<cmd>Telescope resume<cr>", desc = "Find resume" },
+		{ prefix .. "w", "<cmd>Telescope grep_string<cr>", desc = "Find word by grep" },
+		{ prefix .. "s", "<cmd>Telescope spell_suggest<cr>", desc = "Find spelling" },
+		{ prefix .. "k", "<cmd>Telescope keymaps<cr>", desc = "Find keymaps" },
 	},
 }
