ed843fa9af7837bc2d3faa61cc4ad54c40c827ee
Emmanuel Chucks <echucks19@gmail.com>
2023-11-04 01:20:06 +0000

    feat(keymaps): handle all keymaps with which-key
    - neater setup
    - better popup name grouping

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,15 +1,10 @@
 -- telescope.lua
 --
 
-local prefix = "<leader>f"
-require("which-key").register({
-	[prefix] = { name = "Find", _ = "which_key_ignore" },
-})
-
 return {
 	"nvim-telescope/telescope.nvim",
 	branch = "0.1.x",
-	cmd = { "Telescope" },
+	event = { "VeryLazy" },
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		-- Fuzzy Finder Algorithm which requires local dependencies to be built.
@@ -26,18 +21,23 @@ return {
 			end,
 		},
 	},
-	keys = {
-		{ "<leader><space>", "<cmd>Telescope git_files<cr>", desc = "Find git files" },
-		{ prefix .. "/", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Find in current buffer" },
-		{ prefix .. "o", "<cmd>Telescope oldfiles<cr>", desc = "Find recent files" },
-		{ prefix .. "f", "<cmd>Telescope find_files<cr>", desc = "Find files" },
-		{ prefix .. "g", "<cmd>Telescope live_grep<cr>", desc = "Find by grep" },
-		{ prefix .. "b", "<cmd>Telescope buffers<cr>", desc = "Find buffers" },
-		{ prefix .. "d", "<cmd>Telescope diagnostics<cr>", desc = "Find diagnostics" },
-		{ prefix .. "h", "<cmd>Telescope help_tags<cr>", desc = "Find help" },
-		{ prefix .. "r", "<cmd>Telescope resume<cr>", desc = "Find resume" },
-		{ prefix .. "w", "<cmd>Telescope grep_string<cr>", desc = "Find word by grep" },
-		{ prefix .. "s", "<cmd>Telescope spell_suggest<cr>", desc = "Find spelling" },
-		{ prefix .. "k", "<cmd>Telescope keymaps<cr>", desc = "Find keymaps" },
-	},
+	config = function()
+		local builtin = require("telescope.builtin")
+		require("which-key").register({
+			["<leader>f"] = {
+				name = "Find",
+				["/"] = { builtin.live_grep, "Find by grep" },
+				f = { builtin.find_files, "Find files" },
+				g = { builtin.git_files, "Find git files" },
+				b = { builtin.buffers, "Find buffers" },
+				d = { builtin.diagnostics, "Find diagnostics" },
+				h = { builtin.help_tags, "Find help" },
+				r = { builtin.resume, "Find resume" },
+				w = { builtin.grep_string, "Find word by grep" },
+				s = { builtin.spell_suggest, "Find spelling" },
+				k = { builtin.keymaps, "Find keymaps" },
+				o = { builtin.oldfiles, "Find recent files" },
+			},
+		})
+	end,
 }
