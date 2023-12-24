951f0aac86aa80e0af0e9550afb910f840710c75
Emmanuel Chucks <echucks19@gmail.com>
2023-12-24 19:08:56 +0000

    feat(formatting,linting): replace none ls
    - none ls was buggy and would stop working after a while

---
+++ lua/plugins/formatting.lua
@@ -0,0 +1,33 @@
+-- formatting.lua
+--
+
+return {
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	opts = {
+		formatters_by_ft = {
+			javascript = { "eslint_d", "prettierd" },
+			javascriptreact = { "eslint_d", "prettierd" },
+			typescript = { "eslint_d", "prettierd" },
+			typescriptreact = { "eslint_d", "prettierd" },
+			astro = { "eslint_d", "prettierd" },
+			json = { "prettierd" },
+			jsonc = { "prettierd" },
+			markdown = { "prettierd" },
+			lua = { "stylua" },
+		},
+
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_fallback = true,
+		},
+
+		formatters = {
+			eslint_d = {
+				condition = function()
+					return vim.fn.glob(vim.fn.getcwd() .. "/.eslintrc*") ~= ""
+				end,
+			},
+		},
+	},
+}
