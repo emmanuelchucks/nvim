16ffa653ff444164fe79c29514cb1a542e20ca40
Emmanuel Chucks <echucks19@gmail.com>
2023-12-24 19:08:56 +0000

    feat(formatting,linting): replace none ls
    - none ls was buggy and would stop working after a while

---
+++ lua/plugins/linting.lua
@@ -0,0 +1,23 @@
+-- linting.lua
+--
+
+return {
+	"mfussenegger/nvim-lint",
+	event = { "BufReadPost", "BufWritePost", "InsertLeave", "TextChanged" },
+	config = function(opts)
+		require("lint").linters_by_ft = {
+			javascript = { "eslint_d" },
+			javascriptreact = { "eslint_d" },
+			typescript = { "eslint_d" },
+			typescriptreact = { "eslint_d" },
+			astro = { "eslint_d" },
+		}
+
+		vim.api.nvim_create_autocmd(opts.event, {
+			group = vim.api.nvim_create_augroup("lint", { clear = true }),
+			callback = function()
+					require("lint").try_lint()
+			end,
+		})
+	end,
+}
