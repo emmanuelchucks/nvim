da9373f0dc1a492019ff1b1638e0955fcc722c84
Emmanuel Chucks <echucks19@gmail.com>
2024-02-28 01:13:28 +0000

    refactor(treesitter): remove unused code
    - due to using mini.ai

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,39 +5,28 @@ return {
 	-- Highlight, edit, and navigate code
 	"nvim-treesitter/nvim-treesitter",
 	event = { "BufReadPost", "BufNewFile", "BufWritePre" },
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter-textobjects",
-		"nvim-treesitter/nvim-treesitter-context",
-	},
 	build = ":TSUpdate",
 	config = function()
-		vim.defer_fn(function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = {
-					"css",
-					"html",
-					"http",
-					"json",
-					"jsonc",
-					"lua",
-					"markdown",
-					"rust",
-					"toml",
-					"tsx",
-					"typescript",
-					"yaml",
-				},
-				highlight = { enable = true },
-				indent = { enable = true },
-				incremental_selection = {
-					enable = true,
-					keymaps = {
-						init_selection = "<c-space>",
-						node_incremental = "<c-space>",
-					},
-				},
-			})
-		end, 0)
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"css",
+				"html",
+				"http",
+				"json",
+				"jsonc",
+				"lua",
+				"markdown",
+				"rust",
+				"toml",
+				"tsx",
+				"typescript",
+				"yaml",
+			},
+			-- Autoinstall languages that are not installed
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
 
 		-- Support mdx files
 		vim.filetype.add({ extension = { mdx = "markdown.mdx" } })
