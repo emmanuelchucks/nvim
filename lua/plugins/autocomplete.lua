e82374e76222fafce206e69226d612dc46975f15
Emmanuel Chucks <echucks19@gmail.com>
2024-03-04 23:20:50 +0000

    style(autocomplete): use lowercase for commands

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -57,13 +57,13 @@ return {
 					select = true,
 				}),
 				-- <c-l> will move you to the right of each of the expansion locations.
-				["<C-l>"] = cmp.mapping(function()
+				["<c-l>"] = cmp.mapping(function()
 					if luasnip.expand_or_locally_jumpable() then
 						luasnip.expand_or_jump()
 					end
 				end, { "i", "s" }),
 				-- <c-h> is similar, except moving you backwards.
-				["<C-h>"] = cmp.mapping(function()
+				["<c-h>"] = cmp.mapping(function()
 					if luasnip.locally_jumpable(-1) then
 						luasnip.jump(-1)
 					end
