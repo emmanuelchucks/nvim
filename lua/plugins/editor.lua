6cc3ca8c7859ecaa528613a65d1c1bf52bf47572
Emmanuel Chucks <echucks19@gmail.com>
2024-03-16 20:56:06 +0000

    feat(editor): remove nvim rest
    - can't use with form
    - unresponsive on github issue

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -28,25 +28,4 @@ return {
 		main = "ibl",
 		opts = {},
 	},
-
-	{
-		"rest-nvim/rest.nvim",
-		ft = "http",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("rest-nvim").setup({
-				result_split_horizontal = true,
-				result_split_in_place = true,
-			})
-
-			require("which-key").register({
-				["<leader>r"] = {
-					name = "Rest",
-					r = { "<Plug>RestNvim", "Run request" },
-					p = { "<Plug>RestNvimPreview", "Run preview" },
-					l = { "<Plug>RestNvimLast", "Run last request" },
-				},
-			})
-		end,
-	},
 }
