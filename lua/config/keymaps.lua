7379b8c43932ca35ffb1c2b940612c1eca645fa3
Emmanuel Chucks <echucks19@gmail.com>
2024-08-06 22:04:35 +0000

    feat(keymaps): allow jumping to buffers with which-key

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,6 +7,7 @@ return {
 	event = { "VeryLazy" },
 	config = function()
 		local wk = require("which-key")
+		local extras = require("which-key.extras")
 
 		wk.setup({
 			icons = {
@@ -14,8 +15,9 @@ return {
 			},
 			spec = {
 				{ "<space>", "<nop>", mode = { "n", "v" } },
-				{ "<leader>e", vim.diagnostic.open_float, desc = "Open floating diagnostic message" },
 				{ "<leader>w", proxy = "<c-w>", group = "Windows" },
+				{ "<leader>b", expand = extras.expand.buf, group = "Buffers" },
+				{ "<leader>e", vim.diagnostic.open_float, desc = "Open floating diagnostic message" },
 			},
 			triggers = {
 				{ "<auto>", mode = "nixsotc" },
