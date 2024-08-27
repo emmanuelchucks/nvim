babd680eee58b80219c995f235b530da00b7e550
Emmanuel Chucks <echucks19@gmail.com>
2024-08-27 15:40:17 +0000

    refactor(coding): refine keymaps to match muscle memory

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -110,7 +110,6 @@ return {
 		dependencies = {
 			"MunifTanjim/nui.nvim",
 			"nvim-lua/plenary.nvim",
-			"nvim-treesitter/nvim-treesitter",
 		},
 		ft = "hurl",
 		config = function()
@@ -125,13 +124,9 @@ return {
 
 			wk.add({
 				-- Run API request
-				{ "<leader>A", "<cmd>HurlRunner<CR>", desc = "Run All requests" },
-				{ "<leader>a", "<cmd>HurlRunnerAt<CR>", desc = "Run Api request" },
-				{ "<leader>te", "<cmd>HurlRunnerToEntry<CR>", desc = "Run Api request to entry" },
-				{ "<leader>tm", "<cmd>HurlToggleMode<CR>", desc = "Hurl Toggle Mode" },
-				{ "<leader>tv", "<cmd>HurlVerbose<CR>", desc = "Run Api in verbose mode" },
-				-- Run Hurl request in visual mode
-				{ "<leader>h", ":HurlRunner<CR>", desc = "Hurl Runner", mode = "v" },
+				{ "<leader>rr", "<cmd>HurlRunner<CR>", desc = "Run API request" },
+				{ "<leader>rR", "<cmd>HurlRunnerAt<CR>", desc = "Run all API requests" },
+				{ "<leader>rv", "<cmd>HurlVerbose<CR>", desc = "Run API in verbose mode" },
 			})
 		end,
 	},
