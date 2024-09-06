492c1d66da5e548d47df53faef1a32a03c743df4
Emmanuel Chucks <echucks19@gmail.com>
2024-09-06 22:56:07 +0000

    feat(coding): add sql support with vim-dadbod, UI and completions

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -151,4 +151,18 @@ return {
 			})
 		end,
 	},
+
+	{
+		"tpope/vim-dadbod",
+		dependencies = {
+			"kristijanhusak/vim-dadbod-ui",
+			"kristijanhusak/vim-dadbod-completion",
+		},
+		cmd = {
+			"DBUI",
+			"DBUIToggle",
+			"DBUIAddConnection",
+			"DBUIFindBuffer",
+		},
+	},
 }
