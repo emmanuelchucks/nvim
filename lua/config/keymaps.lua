936762e49001f54b445afe10e1d82f01b9d67b92
Emmanuel Chucks <echucks19@gmail.com>
2025-01-28 19:40:37 +0000

    feat(plugins): move terminal into own plugin file
    Add keymap for toggling terminals
    Add keymap to expand a terminal into floating window

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,17 +1,6 @@
 -- keymaps.lua
 --
 
-local open_terminals = function()
-	vim.cmd.vnew()
-	vim.cmd.terminal()
-	vim.cmd.wincmd("H")
-	vim.api.nvim_win_set_width(0, 50)
-	vim.cmd.new()
-	vim.cmd.terminal()
-	vim.cmd.split()
-	vim.cmd.terminal()
-end
-
 return {
 	"folke/which-key.nvim",
 	event = { "VeryLazy" },
@@ -28,9 +17,6 @@ return {
 				{ "<leader>w", proxy = "<c-w>", group = "Windows" },
 				{ "<leader>b", expand = extras.expand.buf, group = "Buffers" },
 				{ "<leader>e", vim.diagnostic.open_float, desc = "Open floating diagnostic message" },
-				-- Terminal
-				{ "<leader>t", open_terminals, desc = "Open terminal" },
-				{ "<esc><esc>", "<c-\\><c-n>", desc = "Escape to normal mode", mode = "t", hidden = true },
 			},
 			triggers = {
 				{ "<auto>", mode = "nixsotc" },
