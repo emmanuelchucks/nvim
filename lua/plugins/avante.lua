08934508b775eb06640510ab52beca145bc8e67c
Emmanuel Chucks <echucks19@gmail.com>
2025-04-25 07:06:28 +0000

    refactor(avante): use gemini 2.5 pro in openrouter

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -94,7 +94,7 @@ return {
 					__inherited_from = "openai",
 					endpoint = "https://openrouter.ai/api/v1",
 					api_key_name = "OPENROUTER_API_KEY",
-					model = "deepseek/deepseek-r1",
+					model = "google/gemini-2.5-pro-preview-03-25",
 				},
 			},
 			disabled_tools = {
