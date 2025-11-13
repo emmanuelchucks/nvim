e140fcd915e013f17fcd7623ac985921c75ecec2
Emmanuel Chucks <echucks19@gmail.com>
2025-11-13 17:09:56 +0000

    feat(treesitter): add textobjects keymaps

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -42,6 +42,18 @@ return {
 				indent = {
 					enable = true,
 				},
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true,
+						keymaps = {
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["ac"] = "@class.outer",
+							["ic"] = "@class.inner",
+						},
+					},
+				},
 			})
 
 			-- Support dotenv files
